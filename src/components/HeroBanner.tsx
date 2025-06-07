399099711a34d6a5c31c29aa68ede5a88bc88ca1
Susant Paudel <techbizznepal@gmail.com>
2025-06-08 02:18:23 +0545

    home page design

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -3,8 +3,8 @@
 export default function HeroBanner({phone}: { phone: string }) {
     return (
         <div className="flex items-start flex-col gap-y-4 px-12 lg:px-72 md:px-36 py-8">
-            <h1 className="text-8xl font-bold text-start py-4 text-primary">Professional Cleaning Services</h1>
-            <p className="text-start text-gray-600 text-4xl">Offering Top-quality cleaning for homes, businesses, NDIS
+            <h1 className="text-6xl lg:text-8xl font-bold text-start py-4 text-primary">Professional Cleaning Services</h1>
+            <p className="text-start text-gray-600 text-md md:text-3xl lg:text-4xl">Offering Top-quality cleaning for homes, businesses, NDIS
                 participants, and Airbnb properties</p>
             <div className="bg-slate-500 w-44 h-12 flex items-center justify-center rounded-md">
                 <a href={`tel:${phone}`} className="text-2xl text-white">Call Now</a>
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -8,12 +8,12 @@ interface ITopNavProps {
 
 export default function TopNav({navigations}: ITopNavProps) {
     return (
-        <div id="nav" className="flex justify-between px-12 lg:px-64 md:px-36 py-2 items-center">
+        <div id="nav" className="flex flex-col justify-between px-12 lg:px-64 md:px-36 py-2 items-center md:flex-row gap-y-2">
             <div>
                 <Image src="/logo.png" alt="Hygiene Angel Logo" width={16 * 8.2} height={9 * 8.2} priority={true}
                        className="w-auto h-auto"/>
             </div>
-            <nav className="flex space-x-28 ">
+            <nav className="flex-row space-x-8 lg:space-x-28 ">
                 {navigations.map((key) => (
                     <a key={key} href={`#${key}`}
                        className="text-gray-700 hover:text-blue-500 font-semibold hover:border-b-2 border-b-primary py-2">{key.toUpperCase()}</a>
